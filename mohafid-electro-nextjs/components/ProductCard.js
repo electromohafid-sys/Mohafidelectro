c584907fc93344c995c209ba8d8b35cd26916f52
electromohafid-sys <electromohafid@gmail.com>
2026-09-09 21:24:32 +0100

    Update ProductCard.js
    New full code , syntex error

--- mohafid-electro-nextjs/components/ProductCard.js
+++ mohafid-electro-nextjs/components/ProductCard.js
@@ -21,10 +21,9 @@ export default function ProductCard({ product }) {
           </svg>
         )}
       </div>
-      </div>
       <h3 className="font-bold mb-1">{product.name}</h3>
       <div className="text-gray-400 text-xs font-mono mb-2" dir="ltr">{product.spec}</div>
-      <div className="font-mono font-semibold text-lg">{Number(product.price).toLocaleString('en-US')} درهم</div>
+      <div className="font-mono font-semibold text-lg">{Number(product.price).toLocaleString('en-US')} ج.م</div>
     </Link>
   );
-}
+          }
